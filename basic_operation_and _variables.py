1f812e71fe5f8b390c02dabfa1cb48a5966d2931
Zainab <zainab.t.ojeleye@gmial.com>
2024-11-19 21:23:36 +0100

    First commit

---
+++ basic_operation_and _variables.py
@@ -0,0 +1,43 @@
+#operations
+num_1 = 20
+num_2 = 4
+
+sum = num_1 + num_2
+print(sum)
+
+difference = num_1 - num_2
+print(difference)
+
+product = num_1 * num_2
+print(product)
+
+try:
+    division = num_1 / num_2
+    print(division)
+except ZeroDivisionError: 
+    print("division by zero")
+    print("cannot be divided")
+finally:
+    print("done")
+
+
+
+
+#variables  
+num = int(23.0)     #integer
+print(num)
+
+num = float(23)     #float
+print(num)
+
+word = "Happy Birthday"      #string
+print(word)
+
+
+cold = True       #Boolean
+hot = False
+print(cold)
+print(hot)
+
+
+
